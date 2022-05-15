a7d7988d7c421ecebae9481b066dc02313cabc90
Vinh Le <lethanhvinh95@gmail.com>
2022-05-15 10:42:12 +0300

    refactor using Post model and Awaited type

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,27 +1,16 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, Link } from "@remix-run/react";
+import { getPosts } from "~/models/post.server";
 
-type Post = {
-  slug: string
-  title: string
-}
-
+// New Awaited type to model async operations
+// See https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-5.html#the-awaited-type-and-promise-improvements
 type LoaderData = {
-  posts: Post[]
+  posts: Awaited<ReturnType<typeof getPosts>>
 }
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
 
