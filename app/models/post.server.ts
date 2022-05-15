1418a6acc41927d7e279c6f75ed293b23955a844
Vinh Le <lethanhvinh95@gmail.com>
2022-05-15 10:49:23 +0300

    add prisma model and seed db for posts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,10 @@
+import { prisma } from '~/db.server'
+
 type Post = {
   slug: string;
   title: string;
 }
 
 export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ]
+  return prisma.post.findMany()
 }
